1370899269fbeaec82d82ee4e287627c7f592309
Fan Huang <fan.huang.33@gmail.com>
2018-12-17 17:36:42 -0500

    added django registration redux to pipfile and settings

--- questionbox/settings.py
+++ questionbox/settings.py
@@ -31,13 +31,13 @@ AUTH_USER_MODEL = 'questions.User'
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'questions',
 ]
 
