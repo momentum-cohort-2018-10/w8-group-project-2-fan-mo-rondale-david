b3e81feaa8da336240b787a67402afdd13b16753
BarstoolsDreamers <david@davidlndean.com>
2018-12-17 11:36:45 -0500

    Question and Answer and Timestamp models

--- questions/models.py
+++ questions/models.py
@@ -5,3 +5,19 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 
     USERNAME_FIELD = 'username'
+
+
+class Timestamp(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Question(Timestamp):
+    title = models.CharField(max_length=255, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+
+
+class Answer(Timestamp):
+    title = models.CharField(max_length=255, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
