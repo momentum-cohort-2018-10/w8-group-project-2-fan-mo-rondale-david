70e1d68b4933b9ba892a5aa70daae72e3b5d38d4
Mo Glatz <meagabeth@icloud.com>
2018-12-17 13:48:20 -0500

    Revert "Add registration & login links to nav bar, create BASIC home page to show"
    This reverts commit 394536f22e75920dba25e60a36d57f4db61608ed.

--- questionbox/urls.py
+++ questionbox/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from questions import views
+from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='home'),
     path('admin/', admin.site.urls),
-    path('accounts/', include('registration.backends.simple.urls'))
 ]
