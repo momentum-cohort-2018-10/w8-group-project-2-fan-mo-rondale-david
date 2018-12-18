e343de4b52a210c9be291a4bdd1d7c8224885d30
Mo Glatz <meagabeth@icloud.com>
2018-12-18 14:51:36 -0500

    Redirect to home page after successful signup

--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            form = UserCreationForm()
+        return render(request, 'registration_form.html', {
+            'form': form
+        })
 
 
