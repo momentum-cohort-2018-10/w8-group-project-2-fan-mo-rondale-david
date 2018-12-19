f2202c2780b284b48152c899624bec563e35e9c6
Mo Glatz <meagabeth@icloud.com>
2018-12-19 15:17:39 -0500

    Add button to ask question, opens modal, click 'x' to close modal

--- questions/static/js/script.js
+++ questions/static/js/script.js
@@ -1,6 +1,22 @@
+/* globals */
+
 document.querySelector('.navbar-burger').addEventListener('click', toggleNavBar)
 
 function toggleNavBar(){
     this.classList.toggle('is-active');
     document.querySelector('.navbar-menu').classList.toggle('is-active');
+}
+
+
+document.getElementById('ask-question').addEventListener('click', askQuestionModal)
+
+function askQuestionModal(){
+    modal.style.display = "block";
+}
+
+
+document.getElementById('close-modal').addEventListener('click', closeModal)
+
+function closeModal(){
+    modal.style.display = "none";
 }
