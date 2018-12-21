6d6cc59960bc1336c26e8ce700b1c399478611bf
Fan Huang <fan.huang.33@gmail.com>
2018-12-21 13:32:39 -0500

    fixed merge issue

--- questions/views.py
+++ questions/views.py
@@ -10,18 +10,6 @@ class QuestionListView(ListView):
     paginate_by = 10
     template_name = 'index.html'
 
-
-def register(request):
-    """
-    Redirects to home page upon successful user registration.
-    """
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            login(request, user)
-            return redirect('home')
     def get_queryset(self):
         if self.request.user.is_authenticated:
             user_id = self.request.user.id
@@ -34,26 +22,13 @@ def register(request):
                 ).prefetch_related('answers')
         else:
             queryset = Question.objects.all().prefetch_related('answers')
-
         return queryset
 
-def login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            login(request, user)
-            return redirect('account_home')
-        else:
-            form = AuthenticationForm()
-        return render(request, 'login.html', {
-            'form': form
-        })
 
 @login_required
 def profile(request):
     return render(request, 'registration/profile.html')
+
 
 def edit_profile(request):
     if request.method == 'POST':
@@ -66,6 +41,7 @@ def edit_profile(request):
         form = EditProfileForm(instance=request.user)
         args = {'form': form}
         return render(request, 'registration/edit_profile.html', args)
+
 
 def change_password(request):
     if request.method == 'POST':
