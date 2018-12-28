3a6cc6982764b1b7eb0af3bdc3c9995794f5d77a
Fan Huang <fan.huang.33@gmail.com>
2018-12-28 05:09:09 -0500

    separate production file

--- questionbox/settings.py
+++ questionbox/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -167,5 +166,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = 'home'
-
-django_heroku.settings(locals())
